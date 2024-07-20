325270476e5756dbea7f33e0857721675ccde068
Rana Awais <awaiswork803@gmail.com>
2024-07-20 20:28:18 +0500

    fix bug

--- components/Dashboard/Styling/Global/Colors/ColorList.js
+++ components/Dashboard/Styling/Global/Colors/ColorList.js
@@ -27,7 +27,7 @@ const ColorList = ({ selectedColor, onSelect, total,
         }}
         renderItem={(color, index) => (
             <List.Item
-            key={font._uid}
+            key={color._uid}
             onClick={() => onSelect(color)}
             style={{
                 color: colorText,
