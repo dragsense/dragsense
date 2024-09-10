66fd1c7ec7be3bdbb9632013a9c4cd870bb3a768
Rana Awais <awaiswork803@gmail.com>
2024-09-11 02:31:47 +0500

    ..

--- node-website/src/client/components/index.jsx
+++ node-website/src/client/components/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-
 import ComplexLogics from './ComplexLogics';
-
 
 export default function AutocodeClient({ children, name, getStateValue, getPropValue, updateProp, updateState }) {
 
